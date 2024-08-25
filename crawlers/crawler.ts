6176a7edd295b4949fcc855d1e4da168842781cb
Hieu Duong <hieuduong21092002@outlook.com>
2024-08-26 01:31:40 +0700

    feat: add another type of extract function
    The extract function now become an object with 2 different functions extracting 2 different type of link.

--- crawlers/crawler.ts
+++ crawlers/crawler.ts
@@ -15,17 +15,47 @@ type PageLink = {
 
 type ProductLink = {
   type: "product";
+  result?: unknown;
 } & BaseLink;
 
 type CrawlLink = PageLink | ProductLink;
 
-type Request<link extends CrawlLink> = {
-  link: link;
-  info: APIWebsiteInfo<unknown, unknown>;
+type ExtractFunction<RawType> =
+  | DefaultExtractFunction<RawType>
+  | {
+      page: ExtractPageFunction;
+
+      product: ExtractProductFunction<RawType>;
+    };
+
+type DefaultExtractFunction<RawType> = (
+  link: CrawlLink,
+  response: Response
+) => Promise<{
+  list: ParseInput<RawType, unknown>[];
+  links: CrawlLink[];
+  pages?: number;
+}>;
+
+type ExtractPageFunction = (
+  link: PageLink,
+  response: Response
+) => Promise<{
+  links: CrawlLink[];
+  pages?: number;
+}>;
+
+type ExtractProductFunction<RawType> = (
+  link: ProductLink,
+  response: Response
+) => Promise<ParseInput<RawType, unknown>[]>;
+
+type ParseInput<RawType, ReturnType> = {
+  raw: RawType;
+  result?: ReturnType;
 };
 
 type Result = {
-  info: APIWebsiteInfo<unknown, unknown>;
   product: Products;
   list: unknown[];
 };
@@ -47,36 +77,28 @@ interface APIWebsiteInfo<RawType, ReturnType> {
    * Create the URL to crawl data from the product enum.
    * @param product The product enum to crawl from.
    */
-  path(product: Products, page: number): PageLink | null;
+  path(product: Products, page: number): CrawlLink | null;
 
   /**
    * Extract the data list from the response object.
    * @param response The response object to extract data.
    */
-  extract(
-    link: CrawlLink,
-    response: Response
-  ): Promise<{
-    list: RawType[];
-    links: CrawlLink[];
-    pages: number | null;
-  }>;
+  extract: ExtractFunction<RawType>;
 
   /**
    * Parse each item from the result of the extract function to the useful data.
    * @param raw The raw data object to parse from.
    */
-  parse(raw: RawType, product?: Products): ReturnType;
+  parse(input: Required<ParseInput<RawType, ReturnType>>): ReturnType;
 }
 
-const WebsitePerFetch = 3;
 const FetchEachLoop = 10;
 const DelayTime = 5000;
 
 class Crawler {
+  private static info: APIWebsiteInfo<unknown, unknown>;
   private static dataPath = path.join(".", "data");
-  private static websites: APIWebsiteInfo<unknown, unknown>[] = [];
-  private static requests: Request<CrawlLink>[] = [];
+  private static requests: CrawlLink[] = [];
   private static errors: { link: CrawlLink; error: unknown }[] = [];
 
   /**
@@ -91,7 +113,10 @@ class Crawler {
       "path" in object &&
       typeof object["path"] == "function" &&
       "extract" in object &&
-      typeof object.extract == "function" &&
+      (typeof object.extract == "function" ||
+        (typeof object.extract == "object" &&
+          object.extract.page &&
+          object.extract.product)) &&
       "parse" in object &&
       typeof object.parse == "function"
     );
@@ -102,34 +127,36 @@ class Crawler {
    * @param info The info to crawl.
    */
   static add(info: APIWebsiteInfo<unknown, unknown>): void {
-    this.websites.push(info);
+    this.info = info;
   }
 
   /**
    * The crawl function.
    * Call this to start the crawling process.
    */
-  static async crawl() {
-    while (this.websites.length > 0) {
-      this.start();
-
-      const result: Result[] = [];
-
-      while (this.requests.length > 0) {
-        const promises = this.requests
-          .splice(0, FetchEachLoop)
-          .map((link) => this.fetchRequest(link));
-
-        result.push(
-          ...(await Promise.all(promises)).filter((result) => result != null)
-        );
-
-        await new Promise((resolve) => setTimeout(resolve, DelayTime));
-      }
-
-      const data = this.map(result);
-      this.save(data);
+  static async crawl(products?: Products[]) {
+    if (!this.info) {
+      throw new Error("No website info specified");
     }
+
+    this.start(products);
+
+    const result: Result[] = [];
+
+    while (this.requests.length > 0) {
+      const promises = this.requests
+        .splice(0, FetchEachLoop)
+        .map((link) => this.fetchRequest(link));
+
+      result.push(
+        ...(await Promise.all(promises)).filter((result) => result != null)
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, DelayTime));
+    }
+
+    const data = this.map(result);
+    this.save(data);
 
     if (this.errors.length > 0) {
       const date = new Date();
@@ -146,14 +173,16 @@ class Crawler {
    * by adding the new fetch request to the request queue
    * from the website list.
    */
-  private static start() {
-    this.websites.splice(0, WebsitePerFetch).forEach((info) => {
-      Object.values(Products).map((product) => {
-        const link = info.path(product, 1);
-        if (link) {
-          this.requests.push({ link, info });
-        }
-      });
+  private static start(products?: Products[]) {
+    if (!products) {
+      products = Object.values(Products);
+    }
+
+    Object.values(Products).map((product) => {
+      const link = this.info.path(product, 1);
+      if (link) {
+        this.requests.push(link);
+      }
     });
   }
 
@@ -162,10 +191,7 @@ class Crawler {
    * @param param The request object of the process.
    * @returns The result of the process.
    */
-  private static async fetchRequest({
-    link,
-    info,
-  }: Request<CrawlLink>): Promise<Result | null> {
+  private static async fetchRequest(link: CrawlLink): Promise<Result | null> {
     const { url, method, body } = link;
 
     console.log(`fetching ${url}`);
@@ -175,26 +201,35 @@ class Crawler {
         throw new Error(`Response code: ${response.status}`);
       }
 
-      const { list, links, pages } = await info.extract(link, response);
+      let list: ParseInput<unknown, unknown>[] = [],
+        links: CrawlLink[] = [],
+        pages;
+
+      if (typeof this.info.extract === "function") {
+        ({ list, links, pages } = await this.info.extract(link, response));
+      } else if (link.type === "page") {
+        ({ links, pages } = await this.info.extract.page(link, response));
+      } else {
+        list = await this.info.extract.product(link, response);
+      }
 
       if (
         link.type == "page" &&
         pages &&
         (list.length > 0 || links.length > 0)
       ) {
-        this.next({ link, info }, pages);
+        this.next(link, pages);
       }
 
-      this.requests.push(
-        ...links.map((value) => {
-          return { link: value, info };
-        })
-      );
+      this.requests.push(...links);
 
       return {
-        info,
         product: link.product,
-        list: list.map((raw) => info.parse(raw, link.product)),
+        list: list.map(({ raw, result }) => {
+          if (!result) result = {};
+
+          return this.info.parse({ raw, result });
+        }),
       };
     } catch (err: unknown) {
       const error = err as Error;
@@ -212,14 +247,11 @@ class Crawler {
    * @param param0 The current request object.
    * @param pages The number of next requests from the current one.
    */
-  private static next({ link, info }: Request<PageLink>, pages: number) {
+  private static next(link: PageLink, pages: number) {
     let nextPage = link.page;
     while (nextPage < pages) {
       nextPage++;
-      this.requests.push({
-        link: info.path(link.product, nextPage)!,
-        info,
-      });
+      this.requests.push(this.info.path(link.product, nextPage)!);
     }
   }
 
@@ -231,10 +263,10 @@ class Crawler {
   private static map(result: Result[]): Record<string, ProductMapping> {
     const data: Record<string, ProductMapping> = {};
 
-    result.forEach(({ info, product, list }) => {
+    result.forEach(({ product, list }) => {
       const key = path.join(
-        info.save,
-        info.domain.replaceAll(/(https:\/\/|www.|\.com|\.vn|\.)+/g, "")
+        this.info.save,
+        this.info.domain.replaceAll(/(https:\/\/|www.|\.com|\.vn|\.)+/g, "")
       );
 
       if (!data[key]) {
