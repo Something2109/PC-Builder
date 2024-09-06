dcbb702e9c883b419276972c0cc8b823c9d6d823
Hieu Duong <hieuduong21092002@outlook.com>
2024-09-06 21:56:38 +0700

    feat: create the edit page for editing article

--- app/introduction/[part]/page.tsx
+++ app/introduction/[part]/page.tsx
@@ -1,32 +1,30 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { Article } from "@/components/article";
+import { RedirectButton } from "@/components/utils/Button";
 import { ArticleType } from "@/models/articles/article";
+import { Database } from "@/models/Database";
+import { Products } from "@/models/interface";
+import { notFound, redirect } from "next/navigation";
 
 export default function PartListPage({ params }: { params: { part: string } }) {
-  const [data, setData] = useState<ArticleType | null>(null);
-  const [error, setError] = useState<{ message: string }>({
-    message: "Loading",
-  });
+  if (Object.values(Products).includes(params.part as Products)) {
+    const data = Database.articles.getIntroduction(params.part as Products);
 
-  useEffect(() => {
-    fetch(`/api/introduction/${params.part}`).then((response) => {
-      if (response.ok) {
-        response.json().then((data: ArticleType) => {
-          setData(data);
-        });
-      } else {
-        response.json().then((data: { message: string }) => {
-          setError(data);
-        });
-      }
-    });
-  }, []);
+    if (!data) {
+      return redirect(`/introduction/${params.part}/edit`);
+    }
 
-  return data ? (
-    <Article article={data as ArticleType} />
-  ) : (
-    <h1>{error.message}</h1>
-  );
+    return (
+      <>
+        <Article article={data as ArticleType} />
+        <RedirectButton
+          href={`/introduction/${params.part}/edit`}
+          className={"font-bold"}
+        >
+          Edit
+        </RedirectButton>
+      </>
+    );
+  }
+
+  return notFound();
 }
