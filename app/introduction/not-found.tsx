fdfb7a6c2cea5eba9abd12b835170a48a0889c08
Hieu Duong <hieuduong21092002@outlook.com>
2024-09-10 00:15:38 +0700

    refactor: remove the old introduction page

--- app/introduction/not-found.tsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-  return <h1>Look's like there's no intro page of this yet</h1>;
-}
--- app/introduction/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function IntroductionPage() {
-  return <h1>Introduction Page</h1>;
-}
