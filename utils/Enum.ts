a0c74305c2bb276a693cdcd609eb36ab0627584c
Hieu Duong <hieuduong21092002@outlook.com>
2024-09-13 16:03:19 +0700

    feat: create enum file

---
+++ utils/Enum.ts
@@ -0,0 +1,35 @@
+enum Topics {
+  INTRODUCTION = "introduction",
+  GUIDE = "guide",
+  FORUM = "forum",
+}
+
+enum Products {
+  CPU = "cpu",
+  GPU = "gpu",
+  GRAPHIC_CARD = "graphic_card",
+  MAIN = "mainboard",
+  RAM = "ram",
+  SSD = "ssd",
+  HDD = "hdd",
+  PSU = "psu",
+  CASE = "case",
+  COOLER = "cooler",
+  AIO = "aio",
+  FAN = "fan",
+}
+
+enum Tables {
+  ARTICLE = "article",
+  CPU = "cpu",
+  GPU = "gpu",
+  GRAPHIC_CARD = "graphic_card",
+  MAIN = "mainboard",
+  RAM = "ram",
+  SSD = "ssd",
+  HDD = "hdd",
+  PSU = "psu",
+  CASE = "case",
+}
+
+export { Topics, Products, Tables };
