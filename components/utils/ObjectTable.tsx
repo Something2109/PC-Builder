120f648dbf2b07b54b8b9657627659e8363273e1
Hieu Duong <hieuduong21092002@outlook.com>
2024-09-27 15:26:34 +0700

    feat: display object inside object using table inside table

--- components/utils/ObjectTable.tsx
+++ components/utils/ObjectTable.tsx
@@ -1,23 +1,34 @@
-export function ObjectTable({ object }: { object?: {} }) {
+import { TableHTMLAttributes } from "react";
+
+export function ObjectTable({
+  object,
+  ...rest
+}: { object?: {} } & TableHTMLAttributes<HTMLTableElement>) {
   if (!object) return undefined;
 
   return (
-    <table>
+    <table {...rest}>
       <tbody>
         {Object.entries(object).map(([key, value], index) => {
           if (value) {
             return (
               <tr
                 key={new Date().getTime() + index}
-                className="border-2 *:p-2 *:rounded-sm"
+                className="first:border-t-0 border-t-2 *:rounded-sm"
               >
-                <td className="border-2 font-bold">{key}</td>
-                <td
-                  className="border-2 break-all"
-                  dangerouslySetInnerHTML={{
-                    __html: value ? value.toLocaleString() : "",
-                  }}
-                />
+                <td className="p-2 font-bold">{key}</td>
+                {typeof value === "object" ? (
+                  <td className="border-l-2 p-0 break-all">
+                    <ObjectTable className="w-full" object={value} />
+                  </td>
+                ) : (
+                  <td
+                    className="border-l-2 p-2 break-all"
+                    dangerouslySetInnerHTML={{
+                      __html: value ? value.toLocaleString() : "",
+                    }}
+                  />
+                )}
               </tr>
             );
           }
