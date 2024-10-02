0d644d5e26d348440eb5aa6fadf12db0a329b070
Hieu Duong <hieuduong21092002@outlook.com>
2024-10-03 02:52:23 +0700

    feat: add the api for adding new products

--- app/api/part/route.ts
+++ app/api/part/route.ts
@@ -20,3 +20,35 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.json(responseList);
 }
+
+export async function POST(request: NextRequest) {
+  try {
+    let { id, ...data } = await request.json();
+
+    const responseList = await Database.parts.set(data, id);
+    if (!responseList) {
+      return NextResponse.json(
+        { message: "There's an error finding filter for your option" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(responseList);
+  } catch (err: any) {
+    console.error(err);
+    let message = (err as Error).message;
+    if (err.errors) {
+      switch (err.errors[0].validatorKey) {
+        case "not_unique":
+          message = "The code name has existed in the database";
+          break;
+        case "isUrl":
+          message = "The url field provided is not an url";
+          break;
+        default:
+          message = "Problem saving into the database";
+      }
+    }
+    return NextResponse.json({ message }, { status: 400 });
+  }
+}
