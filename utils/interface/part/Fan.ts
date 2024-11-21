13f348410bdc93ff00e3a6d3a107bdc1dc7a050f
Hieu Duong <hieuduong21092002@outlook.com>
2024-11-21 23:02:24 +0700

    refactor: use zod to the part interfaces

--- utils/interface/part/Fan.ts
+++ utils/interface/part/Fan.ts
@@ -1,46 +1,54 @@
-import {
-  FanBearings,
-  FanBearingType,
-  FanFormFactors,
-  FanFormFactorType,
-  FilterOptionsType,
-} from "../utils";
+import { FilterOptions, FanBearings, FanFormFactors } from "../utils";
+import { z } from "zod";
 
 namespace Fan {
-  export type Info = {
-    form_factor: FanFormFactorType;
-
-    width: number;
-    length: number;
-    height: number;
-
-    voltage: number;
-
-    speed: number;
-    airflow: number;
-    noise: number;
-    static_pressure: number;
-    bearing: FanBearingType;
-  };
-
-  export const SummaryAttributes = ["form_factor", "bearing", "speed"] as const;
-
-  export const FilterAttributes = ["form_factor", "bearing"] as const;
-
-  export const DefaultFilterOptions: FilterOptions = {
+  export const Schema = z.object({
     form_factor: FanFormFactors,
+
+    width: z.number(),
+    length: z.number(),
+    height: z.number(),
+
+    voltage: z.number(),
+
+    speed: z.number(),
+    airflow: z.number(),
+    noise: z.number(),
+    static_pressure: z.number(),
     bearing: FanBearings,
-  };
+  });
+
+  export type Info = z.infer<typeof Schema>;
+
+  export const SummarySchema = Schema.pick({
+    form_factor: true,
+    bearing: true,
+    speed: true,
+  });
+
+  export const SummaryAttributes = SummarySchema.keyof().options;
 
   export type Summarizable = (typeof SummaryAttributes)[number];
 
-  export type Filterables = (typeof FilterAttributes)[number];
+  export type Summary = z.infer<typeof SummarySchema>;
 
-  export type Summary = {
-    [key in Summarizable]: Info[key];
+  export const FilterOptionSchema = z
+    .object({
+      form_factor: FilterOptions(FanFormFactors),
+      bearing: FilterOptions(FanBearings),
+    })
+    .partial();
+
+  export const DefaultFilterOptions: FilterOptions = {
+    form_factor: FanFormFactors.options,
+    bearing: FanBearings.options,
   };
 
-  export type FilterOptions = FilterOptionsType<Info, Filterables>;
+  export const FilterAttributes = FilterOptionSchema.keyof().options;
+
+  export type Filterables = (typeof FilterAttributes)[number];
+
+  export type FilterOptions = z.infer<typeof FilterOptionSchema>;
 }
 
 export default Fan;
--- utils/interface/part/Mainboard.ts
+++ utils/interface/part/Mainboard.ts
@@ -1,56 +1,60 @@
 import {
-  FilterOptionsType,
   MainboardFormFactors,
-  MainboardFormFactorType,
   RAMFormFactors,
-  RAMFormFactorType,
   RAMProtocols,
-  RAMProtocolType,
+  FilterOptions,
 } from "../utils";
+import { z } from "zod";
 
 namespace Mainboard {
-  export type Info = {
-    form_factor: MainboardFormFactorType;
-
-    socket: string;
-
-    ram_form_factor: RAMFormFactorType;
-    ram_protocol: RAMProtocolType;
-    ram_slot: number;
-    expansion_slots: number;
-
-    io_ports: {};
-  };
-
-  export const SummaryAttributes = [
-    "form_factor",
-    "socket",
-    "ram_form_factor",
-    "ram_protocol",
-  ] as const;
-
-  export const FilterAttributes = [
-    "form_factor",
-    "socket",
-    "ram_form_factor",
-    "ram_protocol",
-  ] as const;
-
-  export const DefaultFilterOptions: FilterOptions = {
+  export const Schema = z.object({
     form_factor: MainboardFormFactors,
+
+    socket: z.string(),
+
     ram_form_factor: RAMFormFactors,
     ram_protocol: RAMProtocols,
-  };
+    ram_slot: z.number(),
+    expansion_slots: z.number(),
+
+    io_ports: z.object({}),
+  });
+
+  export type Info = z.infer<typeof Schema>;
+
+  export const SummarySchema = Schema.pick({
+    form_factor: true,
+    socket: true,
+    ram_form_factor: true,
+    ram_protocol: true,
+  });
+
+  export const SummaryAttributes = SummarySchema.keyof().options;
 
   export type Summarizable = (typeof SummaryAttributes)[number];
 
-  export type Filterables = (typeof FilterAttributes)[number];
+  export type Summary = z.infer<typeof SummarySchema>;
 
-  export type Summary = {
-    [key in Summarizable]: Info[key];
+  export const FilterOptionSchema = z
+    .object({
+      form_factor: FilterOptions(MainboardFormFactors),
+      socket: FilterOptions(z.string()),
+      ram_form_factor: FilterOptions(RAMFormFactors),
+      ram_protocol: FilterOptions(RAMProtocols),
+    })
+    .partial();
+
+  export const DefaultFilterOptions: FilterOptions = {
+    form_factor: MainboardFormFactors.options,
+    ram_form_factor: RAMFormFactors.options,
+    ram_protocol: RAMProtocols.options,
   };
 
-  export type FilterOptions = FilterOptionsType<Info, Filterables>;
+  export const FilterAttributes = FilterOptionSchema.keyof().options;
+
+  export type Filterables = (typeof FilterAttributes)[number];
+
+  export type FilterOptions = z.infer<typeof FilterOptionSchema>;
 }
 
 export default Mainboard;
