a1542410ecd17f956db3bc9adc6970bb6f85a8ca
Hieu Duong <hieuduong21092002@outlook.com>
2025-01-02 21:59:36 +0700

    feat: create the specific cpu service function

--- controllers/part/service/cpu.service.ts
+++ controllers/part/service/cpu.service.ts
@@ -1,9 +1,22 @@
+import { Includeable } from "sequelize";
 import { Injectable } from "@nestjs/common";
+import { PartInformation } from "@/models/parts/tables/Part";
+import { ModelScopes } from "@/models/interface";
 import Part from "@/utils/interface/part/Parts";
 import { Products } from "@/utils/Enum";
 import CPU from "@/utils/interface/part/CPU";
+import { CPUModel } from "@/models/parts/tables/CPU";
 import GPU from "@/utils/interface/part/GPU";
-import { BaseDetailPartService } from "../interface/service.interface";
+import { GPUModel } from "@/models/parts/tables/GPU";
+import {
+  BaseDetailPartService,
+  PageOptions,
+  SearchOptions,
+} from "../interface/service.interface";
+import {
+  DetailInfoOptions as Options,
+  FilterOptions as Filter,
+} from "@/utils/interface";
 
 type Detail = Part.BasicInfo & {
   [Products.CPU]: CPU.Info;
@@ -13,6 +26,120 @@ type Detail = Part.BasicInfo & {
 @Injectable()
 class CPUService extends BaseDetailPartService<Detail> {
   readonly part = Products.CPU;
+
+  async list(options?: Filter & PageOptions & SearchOptions) {
+    const { part, [this.part]: cpu, gpu } = options ?? {};
+
+    const FilteredPart = PartInformation.scope([
+      ModelScopes.SUMMARY,
+      { method: [ModelScopes.FILTER, { ...part, part: [this.part] }] },
+    ]);
+
+    const include: Includeable[] = [
+      {
+        model: CPUModel.scope([
+          ModelScopes.SUMMARY,
+          { method: [ModelScopes.FILTER, cpu] },
+        ]),
+        required: true,
+      },
+      {
+        model: GPUModel.scope([
+          ModelScopes.SUMMARY,
+          { method: [ModelScopes.FILTER, gpu] },
+        ]),
+        required: options?.gpu === null,
+      },
+    ];
+
+    const { rows, count } = await this.listFromPart(
+      FilteredPart,
+      options ?? {},
+      ...include
+    );
+
+    return { total: count, list: rows.map((value) => value.toJSON()) };
+  }
+
+  async filter(options?: Filter): Promise<Filter> {
+    const result: Filter = super.filter(options) as Filter;
+
+    const { part, gpu } = options ?? {};
+
+    if (gpu !== null) {
+      const FilteredPart = PartInformation.scope({
+        method: [ModelScopes.FILTER, { ...part, part: [this.part] }],
+      });
+      const FilteredGPU = GPUModel.scope({
+        method: [ModelScopes.FILTER, gpu],
+      });
+
+      result[Products.GPU] = await this.filterFromModel(
+        FilteredGPU,
+        gpu ?? {},
+        GPU.FilterAttributes,
+        FilteredPart
+      );
+    }
+
+    return result;
+  }
+
+  async create({ gpu, ...part }: Options): Promise<Detail | string> {
+    const instance = await this.create(part);
+    if (!instance || typeof instance === "string") return instance;
+
+    const id = instance.id;
+
+    if (gpu) {
+      await this.setToModel(GPUModel, gpu, id);
+    }
+
+    return (await this.get(instance.id)) as Detail;
+  }
+
+  async get(id: string): Promise<Detail | null> {
+    const instance = await this.getPart(
+      PartInformation.scope(ModelScopes.DETAIL),
+      id,
+      CPUModel.scope(ModelScopes.DETAIL),
+      GPUModel.scope(ModelScopes.DETAIL)
+    );
+
+    return instance?.toJSON() ?? null;
+  }
+
+  async set(
+    { gpu, ...part }: Options,
+    id: string
+  ): Promise<Detail | string | null> {
+    const instance = await super.set({ ...part, part: this.part }, id);
+    if (!instance || typeof instance === "string") return instance;
+
+    id = instance.id;
+
+    if (gpu) {
+      await this.setToModel(GPUModel, gpu, id);
+    } else if (gpu === null) {
+      await GPUModel.destroy({ where: { id } });
+    }
+
+    return (await this.get(instance.id)) as Detail;
+  }
+
+  async delete(id: string): Promise<Detail | null> {
+    const instance = await this.getPart(
+      PartInformation.scope(ModelScopes.DETAIL),
+      id,
+      CPUModel.scope(ModelScopes.DETAIL),
+      GPUModel.scope(ModelScopes.DETAIL)
+    );
+    if (!instance || typeof instance === "string") return instance;
+
+    instance.destroy();
+
+    return instance?.toJSON() ?? null;
+  }
 }
 
 export { CPUService };
