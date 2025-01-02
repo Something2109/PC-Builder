e93e5a52b6819b19708f2c91a59a4111a8e0ed5f
Hieu Duong <hieuduong21092002@outlook.com>
2025-01-02 21:55:10 +0700

    feat: inject the new detail classes to the main service

--- controllers/part/part.module.ts
+++ controllers/part/part.module.ts
@@ -1,18 +1,57 @@
 import { Module } from "@nestjs/common";
 import { PartController } from "./part.controller";
 import { PartService } from "./part.service";
-import { BasePartService } from "./interface/service.interface";
+import { BaseDetailPartService } from "./interface/service.interface";
+import { CPUService } from "./service/cpu.service";
+import { GPUService } from "./service/gpu.service";
+import { GraphicCardService } from "./service/graphic_card.service";
+import { MainboardService } from "./service/mainboard.service";
+import { RAMService } from "./service/ram.service";
+import { SSDService } from "./service/ssd.service";
+import { HDDService } from "./service/hdd.service";
+import { PSUService } from "./service/psu.service";
+import { CaseService } from "./service/case.service";
+import { CoolerService } from "./service/cooler.service";
+import { AIOService } from "./service/aio.service";
+import { FanService } from "./service/fan.service";
 
 const Service = {
   provide: PartService,
-  useFactory: (...service: BasePartService[]) => {
+  useFactory: (...service: BaseDetailPartService<any>[]) => {
     return new PartService(...service);
   },
-  inject: [],
+  inject: [
+    CPUService,
+    GPUService,
+    GraphicCardService,
+    MainboardService,
+    RAMService,
+    SSDService,
+    HDDService,
+    PSUService,
+    CaseService,
+    CoolerService,
+    AIOService,
+    FanService,
+  ],
 };
 
 @Module({
   controllers: [PartController],
-  providers: [Service],
+  providers: [
+    Service,
+    CPUService,
+    GPUService,
+    GraphicCardService,
+    MainboardService,
+    RAMService,
+    SSDService,
+    HDDService,
+    PSUService,
+    CaseService,
+    CoolerService,
+    AIOService,
+    FanService,
+  ],
 })
 export class PartModule {}
