88fbf56b4247f45c4e516a55046c5265a22c7464
Hieu Duong <hieuduong21092002@outlook.com>
2025-01-12 16:37:50 +0700

    feat: override filter functions of model nested parts

--- controllers/part/service/cpublock.service.ts
+++ controllers/part/service/cpublock.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import {
+  CPUBlockModel,
+  CPUBlockSocketModel,
+} from "@/models/parts/tables/CPUBlock";
+import { ModelScopes } from "@/models/interface";
+import { PartInformation } from "@/models/parts/tables/Part";
+import { FilterOptions } from "@/utils/interface";
 import Part from "@/utils/interface/part/Parts";
 import { Products } from "@/utils/Enum";
 import CPUBlock from "@/utils/interface/part/CPUBlock";
-import { BaseDetailPartService } from "../interface/service.interface";
+import {
+  BaseDetailPartService,
+  SearchOptions,
+} from "../interface/service.interface";
 
 type Detail = Part.BasicInfo & {
   [Products.CPU_BLOCK]: CPUBlock.Info;
@@ -11,6 +21,40 @@ type Detail = Part.BasicInfo & {
 @Injectable()
 class CPUBlockService extends BaseDetailPartService<Detail> {
   readonly part = Products.CPU_BLOCK;
+
+  async filter(
+    options?: FilterOptions & SearchOptions
+  ): Promise<FilterOptions> {
+    let { part, [this.part]: filter } = options ?? {};
+    filter = filter ?? {};
+
+    if (!filter.socket) {
+      const CPUBlockPartInclude = {
+        model: CPUBlockModel.scope({ method: [ModelScopes.FILTER, filter] }),
+        attributes: [],
+        include: [
+          {
+            model: PartInformation.scope({
+              method: [ModelScopes.FILTER, part],
+            }),
+            attributes: [],
+          },
+        ],
+      };
+
+      const result = await CPUBlockSocketModel.findAll({
+        include: CPUBlockPartInclude,
+        attributes: ["socket"],
+        group: ["socket"],
+        order: ["socket"],
+        raw: true,
+      });
+
+      filter.socket = result.map((val) => val.socket);
+    }
+
+    return await super.filter({ part, [this.part]: filter });
+  }
 }
 
 export { CPUBlockService };
