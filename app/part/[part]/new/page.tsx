a54a58290e1d48e9e1717836dd5a9844d89733b7
Hieu Duong <hieuduong21092002@outlook.com>
2025-02-06 01:48:35 +0700

    refactor: edit the input pages using the new components

--- app/part/[part]/new/page.tsx
+++ app/part/[part]/new/page.tsx
@@ -1,23 +1,54 @@
 "use client";
 
-import { PartForm } from "@/components/part/Form";
-import { Products } from "@/utils/Enum";
-import Part from "@/utils/interface/info/Parts";
-import React from "react";
+import { InfoForm, FormContainer } from "@/components/part/Form";
+import PartForm from "@/components/part/input/Part";
+import { Button } from "@/components/utils/Button";
+import {
+  ColumnWrapper,
+  ResponsiveWrapper,
+} from "@/components/utils/FlexWrapper";
+import { Products, Info } from "@/utils/Enum";
+import { InfoLabels, ProductInfo } from "@/utils/interface";
+import { useReducer, useRef } from "react";
 
-export default function PartDetailEditPage({
+export default function PartDetailNewPage({
   params: { part },
 }: {
   params: { part: Products };
 }) {
-  const value: Part.BasicInfo = {
-    part,
-    id: "",
-    name: "",
-    code_name: "",
-    brand: "",
-    series: "",
-  };
+  const SaveLink = useRef(`/api/part/${part}`);
+  const [forms, setForm] = useReducer(
+    (state: FormContainer, info: Info) => ({ ...state, [info]: !state[info] }),
+    ProductInfo[part],
+    (init) => Object.fromEntries(init.map((val) => [val, true]))
+  );
 
-  return <PartForm part={part} defaultValue={value} />;
+  return (
+    <>
+      <PartForm path={SaveLink.current} part={part} />
+      <ResponsiveWrapper className="w-full align-top">
+        <ColumnWrapper className="basis-1/2">
+          <h1 className="text-4xl font-bold">Raw</h1>
+        </ColumnWrapper>
+        <ColumnWrapper className="basis-1/2">
+          {ProductInfo[part].map((info) =>
+            forms[info] ? (
+              <InfoForm
+                key={info}
+                path={SaveLink.current}
+                info={info}
+                remove={setForm}
+              />
+            ) : (
+              <Button
+                key={info}
+                className="w-full"
+                onClick={() => setForm(info)}
+              >{`Add ${InfoLabels[info]} Info`}</Button>
+            )
+          )}
+        </ColumnWrapper>
+      </ResponsiveWrapper>
+    </>
+  );
 }
