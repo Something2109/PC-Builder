9f9e8da90436610aa9da7ca4e2dd6886074b72b0
Hieu Duong <hieuduong21092002@outlook.com>
2025-02-15 04:38:40 +0700

    feat: create the user schema for mapping user info

---
+++ utils/interface/user/User.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { Primitive } from "../utils";
+import { Roles } from "@/utils/Enum";
+
+namespace User {
+  export const SignUpOptions = z.object({
+    username: Primitive.String.min(8),
+    password: Primitive.String.min(8),
+  });
+
+  export type LogInOptions = z.infer<typeof SignUpOptions>;
+
+  export const Information = z.object({
+    name: Primitive.String.nullable(),
+    email: Primitive.String.email().nullable(),
+    role: z.nativeEnum(Roles),
+  });
+
+  export type Information = z.infer<typeof Information>;
+
+  export const Schema = z
+    .object({ id: Primitive.String.uuid() })
+    .extend(SignUpOptions.shape)
+    .extend(Information.shape);
+
+  export type Type = z.infer<typeof Schema>;
+
+  export const FilterOptions = z
+    .object({
+      role: z.array(z.nativeEnum(Roles)),
+    })
+    .partial();
+
+  export type FilterOptions = z.infer<typeof FilterOptions>;
+}
+
+export { User };
