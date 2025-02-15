de179ceadd7803c0b62829d0d8c684af5f2ced61
Hieu Duong <hieuduong21092002@outlook.com>
2025-02-15 07:19:15 +0700

    refactor: extract the module initialization outside of import array

--- controllers/app.module.ts
+++ controllers/app.module.ts
@@ -1,67 +1,76 @@
 import { Module, Logger } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
+import { JwtModule } from "@nestjs/jwt";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { MongooseModule } from "@nestjs/mongoose";
 import { ConnectionOptions } from "@/models/options";
 import { Connection } from "mongoose";
 import { ArticleModule } from "./article/article.module";
 import { CrawlerModule } from "./crawler/crawler.module";
 import { PartModule } from "./part/part.module";
 import { UserModule } from "./user/user.module";
-import { JwtModule } from "@nestjs/jwt";
-import { APP_GUARD } from "@nestjs/core";
 import { AuthGuard } from "./utils/role/role.guard";
+
+// Initiate the JWT resolver module.
+const Jwt = JwtModule.register({
+  global: true,
+  secret: process.env.JWT_SECRET,
+  signOptions: { expiresIn: "2 days" },
+});
+
+// Initiate the Sequelize module.
+const Sequelize = SequelizeModule.forRoot({
+  dialect: "mysql",
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  autoLoadModels: true,
+  logging: (
+    (logger: Logger) => (sql: string, timeout: any) =>
+      setTimeout(() => logger.verbose(sql), timeout ?? 0)
+  )(new Logger("Sequelize")),
+  ...ConnectionOptions,
+});
+
+// Initiate the Mongoose module.
+const Mongo = MongooseModule.forRoot(
+  `mongodb://${process.env.MONGO_HOST}/${process.env.DATABASE_NAME}`,
+  {
+    onConnectionCreate: (connection: Connection) => {
+      const logger = new Logger("Mongodb");
+
+      connection.on("connected", () =>
+        logger.verbose("Mongodb database connected")
+      );
+      connection.on("open", () => logger.verbose("Mongodb database open"));
+      connection.on("disconnected", () =>
+        logger.verbose("Mongodb database disconnected")
+      );
+      connection.on("reconnected", () =>
+        logger.verbose("Mongodb database reconnected")
+      );
+      connection.on("disconnecting", () =>
+        logger.verbose("Mongodb database disconnecting")
+      );
+
+      return connection;
+    },
+  }
+);
 
 @Module({
   imports: [
-    PartModule,
-    CrawlerModule,
-    ArticleModule,
-    UserModule,
     ConfigModule.forRoot(),
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: "2 days" },
-    }),
-    SequelizeModule.forRoot({
-      dialect: "mysql",
-      host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadModels: true,
-      logging: (
-        (logger: Logger) => (sql: string, timeout: any) =>
-          setTimeout(() => logger.verbose(sql), timeout ?? 0)
-      )(new Logger("Sequelize")),
-      ...ConnectionOptions,
-    }),
-    MongooseModule.forRoot(
-      `mongodb://${process.env.MONGO_HOST}/${process.env.DATABASE_NAME}`,
-      {
-        onConnectionCreate: (connection: Connection) => {
-          const logger = new Logger("Mongodb");
-
-          connection.on("connected", () =>
-            logger.verbose("Mongodb database connected")
-          );
-          connection.on("open", () => logger.verbose("Mongodb database open"));
-          connection.on("disconnected", () =>
-            logger.verbose("Mongodb database disconnected")
-          );
-          connection.on("reconnected", () =>
-            logger.verbose("Mongodb database reconnected")
-          );
-          connection.on("disconnecting", () =>
-            logger.verbose("Mongodb database disconnecting")
-          );
-
-          return connection;
-        },
-      }
-    ),
+    Jwt,
+    Sequelize,
+    Mongo,
+    ArticleModule,
+    CrawlerModule,
+    PartModule,
+    UserModule,
   ],
   providers: [
     {
