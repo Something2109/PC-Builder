98bbb257b4984c53b1274288ca8b9e33971378c5
Hieu Duong <hieuduong21092002@outlook.com>
2025-02-18 01:58:40 +0700

    refactor: apply the new components to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
-import Header from "@/components/header";
-import NavigationBar from "@/components/navigation";
 import "@/css/globals.css";
+import Header from "@/components/header";
+import { ThemeBody } from "@/components/body";
 import Footer from "@/components/footer";
 
 export default function RootLayout({
@@ -12,31 +8,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [navbar, setNavbar] = useState(false);
-  const [dark, setDark] = useState(true);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    setNavbar(false); // Close the navigation panel
-  }, [pathname]);
-
   return (
     <html lang="en">
-      <body
-        className={`${
-          dark ? "dark" : ""
-        } transition-all ease-in-out duration-500 delay-0 dark:text-line dark:bg-background overflow-y-scroll`}
-      >
-        <Header
-          setNavbar={() => setNavbar(!navbar)}
-          setDark={() => setDark(!dark)}
-        />
-        <NavigationBar toggle={navbar} />
+      <ThemeBody>
+        <Header />
         <main className="container w-11/12 min-h-screen mx-auto *:my-2">
           {children}
         </main>
         <Footer />
-      </body>
+      </ThemeBody>
     </html>
   );
 }
