2711179cae37d2805e812b424ed1298a90f64404
Hieu Duong <hieuduong21092002@outlook.com>
2025-02-18 01:53:46 +0700

    refactor: merge the navigation bar and the header file together

--- components/header.tsx
+++ components/header.tsx
@@ -1,52 +1,72 @@
 "use client";
 import Link from "next/link";
 import { RowWrapper } from "./utils/FlexWrapper";
-import Image from "next/image";
-import { useContext } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { DarkModeButton } from "./body";
 
-export default function Header({
-  setNavbar,
-  setDark,
-}: {
-  setNavbar: Function;
-  setDark: Function;
-}) {
+export default function Header() {
+  const [navbar, setNavbar] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setNavbar(false); // Close the navigation panel
+  }, [pathname]);
+
   return (
-    <header className="sticky top-0 z-50 flex flex-row h-16 place-items-center justify-between bg-header md:h-20">
-      <Link
-        className="font-bold text-xl m-10 md:text-2xl text-yellow-400"
-        href={"/"}
-      >
-        PC Builder
-      </Link>
-      <RowWrapper className="m-10">
-        <button
-          type="button"
-          title="dark-mode"
-          className=" h-1/2"
-          onClick={() => setDark()}
+    <>
+      <header className="sticky top-0 z-50 flex flex-row h-16 place-items-center justify-between bg-header md:h-20">
+        <Link
+          className="font-bold text-xl m-10 md:text-2xl text-yellow-400"
+          href={"/"}
         >
-          <picture className="aspect-square size-7 flex flex-col">
-            <Image
-              src="/images/icons/night-mode.png"
-              width={40}
-              height={40}
-              alt="dark"
-              className="dark:w-0 transition-all ease-in-out duration-500 delay-0"
-            />
-            <Image
-              src="/images/icons/light-mode.png"
-              width={40}
-              height={40}
-              alt="light"
-              className="w-0 dark:w-12 transition-all ease-in-out duration-500 delay-0"
-            />
-          </picture>
-        </button>
-        <button type="button" className="md:hidden" onClick={() => setNavbar()}>
-          Menu
-        </button>
-      </RowWrapper>
-    </header>
+          PC Builder
+        </Link>
+        <RowWrapper className="m-10">
+          <DarkModeButton />
+          <button
+            type="button"
+            className="md:hidden"
+            onClick={() => setNavbar(!navbar)}
+          >
+            Menu
+          </button>
+        </RowWrapper>
+      </header>
+      <NavigationBar toggle={navbar} />
+    </>
+  );
+}
+
+const transitionClass = "transition-all ease-in-out duration-500 delay-0";
+const smallScreen =
+  "sticky z-50 mx-auto top-16 md:top-20 flex flex-row flex-wrap overflow-y-auto bg-navigation h-fit";
+const mediumScreen = "md:justify-evenly md:max-h-fit";
+
+function NavigationBar({ toggle }: { toggle: boolean }) {
+  return (
+    <nav
+      className={`${transitionClass} ${smallScreen} ${mediumScreen} ${
+        toggle ? "max-h-9" : "max-h-0"
+      }`}
+    >
+      <NavigationButton link="/introduction" title="Introduction" />
+      <NavigationButton link="/guide" title="Guide" />
+      <NavigationButton link="/part" title="Part" />
+      <NavigationButton link="/build" title="Build" />
+    </nav>
+  );
+}
+
+function NavigationButton({ title, link }: { title: string; link: string }) {
+  return (
+    <Link
+      href={link}
+      className={
+        "m-0.5 px-4 py-1 font-medium md:text-2xl hover:m-0 hover:bg-header hover:border-blue-700 hover:border-2"
+      }
+    >
+      {title}
+    </Link>
   );
 }
