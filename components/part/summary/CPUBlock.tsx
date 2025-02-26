0c1a248a5b158fa0f3ddea00652c71b30c20dbfc
Hieu Duong <hieuduong21092002@outlook.com>
2025-02-27 02:49:50 +0700

    style: shorten the display of cpu block's socket list

--- components/part/summary/CPUBlock.tsx
+++ components/part/summary/CPUBlock.tsx
@@ -2,7 +2,13 @@ import CPUBlock from "@/utils/interface/info/CPUBlock";
 import { GenericSummaryCells, InfoSummaryMapping } from "../TableWrapper";
 
 const Components: InfoSummaryMapping<CPUBlock.Info, CPUBlock.Summarizable> = {
-  socket: ({ value }) => value?.join(", "),
+  socket: ({ value }) => {
+    const sockets = value?.join(", ");
+
+    return sockets && sockets?.length > 20
+      ? `${sockets.slice(0, 20)}...`
+      : sockets;
+  },
   plate: ({ value }) => value,
 };
 
