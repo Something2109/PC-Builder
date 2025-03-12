6700ebf4da56e12c247462063f7512f0bf1a9edd
Hieu Duong <hieuduong21092002@outlook.com>
2025-03-12 14:57:59 +0700

    feat: create the toggle component to show and hide children components

---
+++ components/utils/Toggle.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { HTMLAttributes, useState } from "react";
+import { Button } from "./Button";
+import { RowWrapper } from "./FlexWrapper";
+
+export function Toggler({
+  label,
+  defaultToggle,
+  children,
+  ...rest
+}: {
+  label?: string;
+  defaultToggle?: boolean;
+} & HTMLAttributes<HTMLDivElement>) {
+  const [toggle, setToggle] = useState(Boolean(defaultToggle));
+
+  if (!toggle) {
+    return (
+      <Button type="button" onClick={() => setToggle(true)}>
+        {label ?? "Add"}
+      </Button>
+    );
+  }
+
+  return (
+    <RowWrapper {...rest}>
+      {children}
+      <Button type="button" onClick={() => setToggle(false)}>
+        Remove
+      </Button>
+    </RowWrapper>
+  );
+}
+
+export function ToggleButton({
+  label,
+  defaultToggle,
+  children,
+}: {
+  label?: string;
+  defaultToggle?: boolean;
+  children: React.ReactNode;
+}) {
+  const [toggle, setToggle] = useState(Boolean(defaultToggle));
+
+  return (
+    <>
+      <Button type="button" onClick={() => setToggle((prev) => !prev)}>
+        {toggle ? "Close" : label}
+      </Button>
+      {toggle && children}
+    </>
+  );
+}
