6042f33db3c0d7545155d771acd3f6dda0b4a673
Hieu Duong <hieuduong21092002@outlook.com>
2025-04-24 14:04:24 +0700

    refactor: shorten the logic of the Auth Wrapper

--- components/auth.tsx
+++ components/auth.tsx
@@ -18,45 +18,46 @@ import {
   useReducer,
   useState,
 } from "react";
-import { decode } from "jsonwebtoken";
+import { decode, JwtPayload } from "jsonwebtoken";
 import { usePathname, useRouter } from "next/navigation";
-import Link from "next/link";
-import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosError } from "axios";
 
 const AUTH_KEY = "Authorization";
 const LoginPath = "/auth/login";
-const AuthChanger = createContext<ActionDispatch<[string | null]> | null>(null);
+const AuthChanger = createContext<ActionDispatch<[string | null]>>(() => {});
 
 export const AuthContext = createContext<User.JwtPayload | null>(null);
+
+function decodeToken(token: string | null) {
+  const payload = decode(token ?? "") as JwtPayload | null;
+
+  if (!payload) return null;
+
+  if (payload.exp || payload.iat) {
+    const current = new Date().getTime() / 1000;
+
+    if (payload.exp && payload.exp < current) return null;
+
+    if (payload.nbf && payload.nbf > current) return null;
+  }
+
+  return payload.sub as any as User.JwtPayload;
+}
 
 export function AuthWrapper({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useReducer(
     (_: User.JwtPayload | null, curr: string | null) => {
-      try {
-        if (curr) {
-          localStorage.setItem(AUTH_KEY, curr);
+      const userInfo = decodeToken(curr ?? "");
 
-          axios.interceptors.request.use(
-            (config: InternalAxiosRequestConfig) => {
-              const token = localStorage.getItem(AUTH_KEY);
-              if (token) config.headers.Authorization = `Bearer ${token}`;
+      curr && userInfo
+        ? localStorage.setItem(AUTH_KEY, curr)
+        : localStorage.removeItem(AUTH_KEY);
 
-              return config;
-            }
-          );
-
-          return decode(curr) as User.JwtPayload;
-        }
-      } catch {}
-
-      axios.interceptors.request.clear();
-      localStorage.removeItem(AUTH_KEY);
-      return null;
+      return userInfo;
     },
-    null
+    null,
+    () => decodeToken(localStorage?.getItem(AUTH_KEY) ?? "")
   );
-
-  useLayoutEffect(() => setUser(localStorage.getItem(AUTH_KEY)), []);
 
   return (
     <AuthContext value={user}>
@@ -136,7 +137,7 @@ function LogoutButton({ ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
       await axios.post("/api/auth/logout", undefined, {
         withCredentials: true,
       });
-      setUser!(null);
+      setUser(null);
       router.push(LoginPath);
     } catch (err) {
       const error = err as AxiosError;
@@ -166,7 +167,7 @@ export function LoginForm({ pathname }: { pathname?: string }) {
         const response = await axios.post("/api/auth/login", body, {
           withCredentials: true,
         });
-        setUser!(response.data.access_token);
+        setUser(response.data.refresh_token);
         router.push(pathname ?? "/");
       } catch (err) {
         const error = err as AxiosError;
