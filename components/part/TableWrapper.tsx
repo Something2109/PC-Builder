2e4e5f5d1c12b3d81602c45b7da5bd3ef1860ce6
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-07 04:22:40 +0700

    refactor: move the part table related component to a new file

--- components/part/TableWrapper.tsx
+++ components/part/TableWrapper.tsx
@@ -1,45 +1,14 @@
 import {
   FunctionComponent,
-  HTMLAttributes,
   InputHTMLAttributes,
   SelectHTMLAttributes,
   TableHTMLAttributes,
-  TdHTMLAttributes,
 } from "react";
 import { RowWrapper } from "../utils/FlexWrapper";
 import { Button } from "../utils/Button";
 import { VerticalCollapsible } from "../utils/Collapsible";
 import { Toggler } from "../utils/Toggle";
-
-export namespace Table {
-  const tableRow = "border-b-2 last:border-b-0 *:rounded-sm";
-  const tableCell =
-    "border-r-2 last:border-r-0 first:font-bold p-2 [&:has(table)]:p-0";
-
-  export const Row = ({
-    className,
-    ...attr
-  }: HTMLAttributes<HTMLTableRowElement>) => (
-    <tr
-      className={className ? className.concat(" ", tableRow) : tableRow}
-      {...attr}
-    />
-  );
-
-  export const Cell = ({
-    className,
-    ...attr
-  }: TdHTMLAttributes<HTMLTableCellElement>) => (
-    <td
-      className={className ? className.concat(" ", tableCell) : tableCell}
-      {...attr}
-    />
-  );
-}
-
-export type InfoLabel<T extends Record<string, any>> = {
-  [key in keyof Required<T>]: string;
-};
+import { InfoComponent, InfoLabel, InfoComponentObject } from "./utils/Table";
 
 export type InfoSummaryMapping<T extends Record<string, any>> = {
   [key in keyof Required<T>]: FunctionComponent<{ value?: T[key] }>;
@@ -123,83 +92,38 @@ export function GenericFilterBar<T extends Record<string, string[] | number[]>>(
   );
 }
 
-const tableClass = "w-full border-2";
-
-export type InfoDetailMapping<T extends Record<string, any>> = {
-  [key in keyof Required<T>]: FunctionComponent<{ value?: T[key] }>;
-};
-
 export function GenericDetailTable<T extends Record<string, any>>(
-  Components: InfoDetailMapping<T>,
+  Components: InfoComponentObject<T>,
   Labels: InfoLabel<T>
 ) {
-  return ({
-    defaultValue,
-    ...rest
-  }: {
-    defaultValue?: Partial<T>;
-  } & Omit<TableHTMLAttributes<HTMLTableElement>, "defaultValue">) => (
-    <TableWrapper {...rest}>
-      {Object.entries(Components).map(([key, Component]) => {
-        const value = defaultValue ? defaultValue[key] : undefined;
+  const DetailComponent = InfoComponent(Components, Labels, { strict: true });
 
-        if (!value) return undefined;
-
-        return (
-          <TableRowWrapper key={key}>
-            {Labels[key]}
-            <Component value={value} />
-          </TableRowWrapper>
-        );
-      })}
-    </TableWrapper>
-  );
+  return (
+    props: {
+      defaultValue?: Partial<T>;
+    } & Omit<TableHTMLAttributes<HTMLTableElement>, "defaultValue">
+  ) => <DetailComponent {...props} />;
 }
 
-type CustomInputComponent<Value> = FunctionComponent<
-  { value?: Value; defaultValue?: Value } & Omit<
-    InputHTMLAttributes<HTMLInputElement> &
-      SelectHTMLAttributes<HTMLSelectElement>,
-    "defaultValue" | "value"
-  >
->;
-
-export type InfoInputMapping<T extends Record<string, any>> = {
-  [key in keyof Required<T>]: CustomInputComponent<T[key]>;
-};
-
 export function GenericInputTable<T extends Record<string, any>>(
-  Components: InfoInputMapping<T>,
+  Components: InfoComponentObject<T>,
   Labels: InfoLabel<T>,
   transform: (data: FormData) => Partial<T>
 ) {
+  const InputComponent = InfoComponent(Components, Labels);
+
   return ({
     pending,
     onSubmit,
-    defaultValue,
-    ...rest
+    ...props
   }: {
     pending: boolean;
     onSubmit: (data: Partial<T> | null) => void;
     defaultValue?: Partial<T>;
   } & Omit<TableHTMLAttributes<HTMLTableElement>, "defaultValue">) => {
-    defaultValue = defaultValue ?? ({} as T);
-
     return (
       <>
-        <TableWrapper {...rest}>
-          {Object.entries(Components).map(([key, Component]) => (
-            <TableRowWrapper key={key}>
-              {Labels[key]}
-              <Component
-                name={key}
-                title={Labels[key]}
-                placeholder={Labels[key]}
-                defaultValue={defaultValue[key]}
-              />
-            </TableRowWrapper>
-          ))}
-        </TableWrapper>
+        <InputComponent {...props} />
         {pending ? (
           <p className="button border-0">Saving...</p>
         ) : (
@@ -220,37 +144,4 @@ export function GenericInputTable<T extends Record<string, any>>(
       </>
     );
   };
-}
-
-export function TableWrapper({
-  className,
-  children,
-  ...rest
-}: TableHTMLAttributes<HTMLTableElement>) {
-  return (
-    <table
-      className={className ? className.concat(" ", tableClass) : tableClass}
-      {...rest}
-    >
-      <tbody>{children}</tbody>
-    </table>
-  );
-}
-
-export function TableRowWrapper({
-  children,
-  ...rest
-}: { children: React.ReactNode[] } & HTMLAttributes<HTMLTableRowElement>) {
-  return (
-    <Table.Row {...rest}>
-      {[...children].map((child, index) => (
-        <Table.Cell
-          colSpan={index === 0 ? 2 : 1}
-          key={new Date().getTime() + index}
-        >
-          {child}
-        </Table.Cell>
-      ))}
-    </Table.Row>
-  );
 }
