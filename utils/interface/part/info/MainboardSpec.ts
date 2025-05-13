428a3069810046b6582b6af8718867f74638d156
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-14 03:37:10 +0700

    refactor: remove redundant schemas

--- utils/interface/part/info/MainboardSpec.ts
+++ utils/interface/part/info/MainboardSpec.ts
@@ -2,20 +2,6 @@ import { FormFactor, InternalConnectors, Primitive } from "../../utils";
 import { z } from "zod";
 
 namespace MainboardSpec {
-  export const PowerConnectorSchema = z.record(
-    InternalConnectors.Power.Mainboard,
-    Primitive.Number
-  );
-
-  export type PowerConnector = z.infer<typeof PowerConnectorSchema>;
-
-  export const FanConnectorSchema = z.record(
-    InternalConnectors.Fan.Schema,
-    Primitive.Number
-  );
-
-  export type FanConnector = z.infer<typeof FanConnectorSchema>;
-
   export const Schema = z.object({
     form_factor: FormFactor.Mainboard,
 
@@ -26,8 +12,6 @@ namespace MainboardSpec {
     ram_interface: InternalConnectors.RAM,
     ram_slot: Primitive.Number,
 
-    power_connectors: PowerConnectorSchema,
-    fan_connectors: FanConnectorSchema,
     miscelanous_connectors: z.record(
       InternalConnectors.Miscellanous,
       Primitive.Number
@@ -46,8 +30,6 @@ namespace MainboardSpec {
     ram_interface: "RAM Interface",
     ram_slot: "RAM Slots",
 
-    power_connectors: "Power Connectors",
-    fan_connectors: "Fan Connectors",
     miscelanous_connectors: "Misc Connectors",
   };
 }
