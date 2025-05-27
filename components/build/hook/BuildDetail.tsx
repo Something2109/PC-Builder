f96e5f1a5166f42c6d81e9a56369b42e206d8198
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-28 02:56:17 +0700

    feat: create some hooks for build functionality

---
+++ components/build/hook/BuildDetail.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import Build from "@/utils/interface/build";
+import Part from "@/utils/interface/part";
+import { Products } from "@/utils/Enum";
+import {
+  createContext,
+  useActionState,
+  useContext,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
+
+type DetailMapping<T = Part.Detail> = {
+  [key in Products]?: T[];
+};
+
+const ArrayProducts = [
+  Products.GRAPHIC_CARD,
+  Products.RAM,
+  Products.SSD,
+  Products.HDD,
+  Products.FAN,
+];
+
+class BuildDetailBuilder {
+  private readonly details: DetailMapping<Part.Summary>;
+
+  constructor(details: Build.Details<Part.Summary>) {
+    this.details = Object.values(Products).reduce((acc, product) => {
+      if (Array.isArray(details[product])) {
+        acc[product] = details[product];
+      } else {
+        acc[product] = details[product] ? [details[product]] : [];
+      }
+
+      return acc;
+    }, {} as DetailMapping<Part.Summary>);
+  }
+
+  add(product: Products, summary: Part.Summary) {
+    if (
+      !ArrayProducts.includes(product) &&
+      this.details[product] &&
+      this.details[product].length > 1
+    ) {
+      return undefined;
+    }
+
+    if (!this.details[product]) this.details[product] = [];
+
+    this.details[product].push(summary);
+
+    return summary;
+  }
+
+  remove(product: Products, summary: Part.Summary) {
+    if (!this.details[product]) return undefined;
+
+    const index = this.details[product].indexOf(summary);
+    if (index > -1) {
+      this.details[product].splice(index, 1);
+
+      if (this.details[product]) delete this.details[product];
+
+      return summary;
+    }
+
+    return undefined;
+  }
+
+  list(): Build.List {
+    const entries = Object.entries(this.details)
+      .map(([key, values]) => {
+        const list = values.map((val) => val.id);
+
+        if (ArrayProducts.includes(key as Products)) return [key, list];
+
+        return [key, list[0]];
+      })
+      .filter(
+        ([, list]) => list && (Array.isArray(list) ? list.length > 0 : true)
+      );
+
+    return Object.fromEntries(entries);
+  }
+
+  detail(): Build.Details<Part.Summary> {
+    const entries = Object.entries(this.details)
+      .map(([key, list]) => {
+        if (ArrayProducts.includes(key as Products)) return [key, list];
+
+        return [key, list[0]];
+      })
+      .filter(
+        ([, list]) => list && (Array.isArray(list) ? list.length > 0 : true)
+      );
+
+    return Object.fromEntries(entries);
+  }
+}
+
+function useBuildDetails(defaultValue: Build.Details<Part.Summary>) {
+  const builder = useRef(new BuildDetailBuilder(defaultValue));
+  const [{ details, list }, setDetails] = useReducer(
+    () => ({ details: builder.current.detail(), list: builder.current.list() }),
+    { details: builder.current.detail(), list: builder.current.list() }
+  );
+
+  const add = (summary: Part.Summary) => {
+    const result = builder.current.add(summary.part, summary);
+    if (result) setDetails();
+  };
+
+  const remove = (summary: Part.Summary) => {
+    const result = builder.current.remove(summary.part, summary);
+    if (result) setDetails();
+  };
+
+  return [details, list, add, remove] as const;
+}
+
+type BuildContext = {
+  details: Build.Details<Part.Summary>;
+  list: Build.List;
+  add: (p: Part.Summary) => void;
+  remove: (p: Part.Summary) => void;
+};
+
+const BuildPartContext = createContext<BuildContext>({
+  details: {},
+  list: {},
+  add: () => {},
+  remove: () => {},
+});
+
+function BuildProvider({ children }: { children: React.ReactNode }) {
+  const [details, list, add, remove] = useBuildDetails({});
+
+  return (
+    <BuildPartContext.Provider value={{ details, list, add, remove }}>
+      {children}
+    </BuildPartContext.Provider>
+  );
+}
+
+function useBuildContext() {
+  return useContext(BuildPartContext);
+}
+
+export { useBuildDetails, BuildProvider, useBuildContext };
