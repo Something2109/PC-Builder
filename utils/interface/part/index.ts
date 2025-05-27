0bafd5a8a01dea8719c258364140f0ae39306d2d
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-27 08:24:10 +0700

    feat: create infer namespace for inferring type
    the namespace provides types to infer type in part detail with the info and attribute provided

--- utils/interface/part/index.ts
+++ utils/interface/part/index.ts
@@ -88,6 +88,43 @@ namespace Part {
   );
 
   export type Detail = z.infer<typeof Detail>;
+
+  /**
+   * The `Infer` namespace provides types to infer information and attributes
+   * based on the part's {@link Detail} info.
+   */
+  export namespace Infer {
+    /**
+     * Type representing the information type for a given info key.
+     * @template I - The info key from the `Infos` enum.
+     * * This type extracts the type of information associated with the given info key.
+     */
+    export type InfoType<I extends Infos> = NonNullable<
+      Part.Detail[I]
+    > extends Information.Info[I][]
+      ? (Information.Info[I] | undefined)[]
+      : Information.Info[I] | undefined;
+
+    /**
+     * Type representing the attribute type for a given info key and attribute name.
+     * @template I - The info key from the `Infos` enum.
+     * @template A - The attribute name as a string.
+     * * This type extracts the type of the attribute associated with the given info key and attribute name.
+     * * It ensures that if the information is an array, the type is an array of the attribute type.
+     */
+    export type AttributeType<
+      I extends Infos,
+      A extends string
+    > = A extends keyof Information.Info[I]
+      ? NonNullable<Part.Detail[I]> extends Information.Info[I][]
+        ? (Information.Info[I][A] | undefined)[]
+        : Information.Info[I][A] | undefined
+      : undefined;
+  }
+
+  export type Infer<I extends Infos, A extends string = ""> = A extends ""
+    ? Infer.InfoType<I>
+    : Infer.AttributeType<I, A>;
 }
 
 export default Part;
