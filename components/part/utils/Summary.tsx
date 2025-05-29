fe7010ac30e27b3306e1bfc771c852b7aa0ad9cb
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-29 17:35:05 +0700

    feat: create the summary table namespace holding table component

--- components/part/utils/Summary.tsx
+++ components/part/utils/Summary.tsx
@@ -1,6 +1,44 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, HTMLAttributes } from "react";
 import { RowWrapper } from "../../utils/FlexWrapper";
 import { InfoLabel } from "./Table";
+
+export namespace SummaryTable {
+  const tableClass = "w-full border-separate border-spacing-0";
+  const tableHeader =
+    "hidden lg:table-header-group font-bold sticky top-32 bg-white dark:bg-background transition-bg";
+  const tableRow =
+    "grid grid-cols-2 border-b-2 *:p-2 lg:table-row *:lg:border-b-2 hover:rounded-lg hover:bg-line hover:dark:text-background";
+
+  export const Component = ({
+    className,
+    ...rest
+  }: HTMLAttributes<HTMLTableElement>) => (
+    <table
+      className={className ? className.concat(" ", tableClass) : tableClass}
+      {...rest}
+    />
+  );
+
+  export const Head = ({
+    className,
+    ...attr
+  }: HTMLAttributes<HTMLTableSectionElement>) => (
+    <thead
+      className={className ? className.concat(" ", tableHeader) : tableHeader}
+      {...attr}
+    />
+  );
+
+  export const Row = ({
+    className,
+    ...attr
+  }: HTMLAttributes<HTMLTableRowElement>) => (
+    <tr
+      className={className ? className.concat(" ", tableRow) : tableRow}
+      {...attr}
+    />
+  );
+}
 
 export type InfoSummaryMapping<T extends Record<string, any>> = {
   [key in keyof Required<T>]: FunctionComponent<{ value?: T[key] }>;
