550d0fb251dab71252c1a45563574f8e467b75e6
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-30 11:07:43 +0700

    refactor: add new hook and components to the page component

--- app/build/page.tsx
+++ app/build/page.tsx
@@ -1,33 +1,33 @@
-import { BuildProvider } from "@/components/build/hook/BuildDetail";
-import { ProductSelectProvider } from "@/components/build/hook/ProductSelect";
+import { BuildProvider } from "@/components/build/hook/BuildContext";
+import { ProductTypeProvider } from "@/components/build/hook/ProductType";
+import { ValidationProvider } from "@/components/build/hook/Validation";
 import BuildProductList from "@/components/build/List";
 import BuildValidateForm from "@/components/build/Form";
 import BuildProductSummary from "@/components/build/Summary";
-import { VerticalCollapsible } from "@/components/utils/Collapsible";
+import BuildResultList from "@/components/build/Result";
 import { ColumnWrapper, RowWrapper } from "@/components/utils/FlexWrapper";
 
 export default function build() {
   return (
     <BuildProvider>
-      <ProductSelectProvider>
-        <RowWrapper className="w-full">
-          <ColumnWrapper className="w-1/3">
-            <h1 className="text-2xl font-bold mb-4">Build Your PC</h1>
-            <p className="mb-4">
-              Select the components you want to include in your build.
-            </p>
-            <BuildProductList />
-            <BuildValidateForm />
-            <VerticalCollapsible>
-              <h1 className="text-2xl font-bold">Result</h1>
-              <ColumnWrapper></ColumnWrapper>
-            </VerticalCollapsible>
-          </ColumnWrapper>
-          <ColumnWrapper>
-            <BuildProductSummary></BuildProductSummary>
-          </ColumnWrapper>
-        </RowWrapper>
-      </ProductSelectProvider>
+      <ProductTypeProvider>
+        <ValidationProvider>
+          <RowWrapper className="w-full">
+            <ColumnWrapper className="w-1/3">
+              <h1 className="text-2xl font-bold mb-4">Build Your PC</h1>
+              <p className="mb-4">
+                Select the components you want to include in your build.
+              </p>
+              <BuildProductList />
+              <BuildValidateForm />
+              <BuildResultList />
+            </ColumnWrapper>
+            <ColumnWrapper>
+              <BuildProductSummary></BuildProductSummary>
+            </ColumnWrapper>
+          </RowWrapper>
+        </ValidationProvider>
+      </ProductTypeProvider>
     </BuildProvider>
   );
 }
