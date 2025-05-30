c12adcabebb9f9a448d0fc0ab11e364b215a6ca9
Hieu Duong <hieuduong21092002@outlook.com>
2025-05-30 10:28:40 +0700

    fix: add the rowspan for the type column of external port

--- components/part/detail/PartExternalPorts.tsx
+++ components/part/detail/PartExternalPorts.tsx
@@ -1,27 +1,36 @@
 import { Table } from "../utils/Table";
 import PartExternalPorts from "@/utils/interface/part/info/PartExternalPorts";
+import { ExternalPorts } from "@/utils/interface/utils";
 
 export default ({
   defaultValue,
 }: {
   defaultValue: PartExternalPorts.Info[];
-}) => (
-  <Table.Component>
-    <thead>
-      <Table.Row>
-        <Table.Cell>{PartExternalPorts.Label.type}</Table.Cell>
-        <Table.Cell>{PartExternalPorts.Label.name}</Table.Cell>
-        <Table.Cell>{PartExternalPorts.Label.count}</Table.Cell>
-      </Table.Row>
-    </thead>
-    <tbody>
-      {defaultValue.map((val) => (
-        <Table.Row key={`port-${val.name}`}>
-          <Table.Cell>{val.type}</Table.Cell>
-          <Table.Cell>{val.name}</Table.Cell>
-          <Table.Cell>{val.count}</Table.Cell>
+}) => {
+  const groupByType = Object.groupBy(defaultValue, (val) => val.type);
+
+  return (
+    <Table.Component>
+      <thead>
+        <Table.Row>
+          <Table.Cell>{PartExternalPorts.Label.type}</Table.Cell>
+          <Table.Cell>{PartExternalPorts.Label.name}</Table.Cell>
+          <Table.Cell>{PartExternalPorts.Label.count}</Table.Cell>
         </Table.Row>
-      ))}
-    </tbody>
-  </Table.Component>
-);
+      </thead>
+      <tbody>
+        {ExternalPorts.Type.options.map((type) =>
+          groupByType[type]?.map((value, index, arr) => (
+            <Table.Row key={`External-${type}-${value.name}`}>
+              {index === 0 && (
+                <Table.Cell rowSpan={arr.length}>{type}</Table.Cell>
+              )}
+              <Table.Cell>{value.name}</Table.Cell>
+              <Table.Cell>{value.count}</Table.Cell>
+            </Table.Row>
+          ))
+        )}
+      </tbody>
+    </Table.Component>
+  );
+};
