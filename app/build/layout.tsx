1d693858feb799360635aefcb6c576e4ec562e08
Hieu Duong <hieuduong21092002@outlook.com>
2025-06-01 03:29:30 +0700

    refactor: remove the unused component and hook

--- app/build/layout.tsx
+++ app/build/layout.tsx
@@ -1,14 +1,11 @@
 import { BuildProvider } from "@/components/build/hook/BuildContext";
-import { ProductTypeProvider } from "@/components/build/hook/ProductType";
 import { ValidationProvider } from "@/components/build/hook/Validation";
 import { ReactNode } from "react";
 
 export default function build({ children }: { children: ReactNode }) {
   return (
     <BuildProvider>
-      <ProductTypeProvider>
-        <ValidationProvider>{children}</ValidationProvider>
-      </ProductTypeProvider>
+      <ValidationProvider>{children}</ValidationProvider>
     </BuildProvider>
   );
 }
