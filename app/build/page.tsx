3a2deb63dc427ea19d615e24c522ee9f112ba7d6
Hieu Duong <hieuduong21092002@outlook.com>
2025-06-01 03:28:36 +0700

    refactor: remove summary component from the main build page

--- app/build/page.tsx
+++ app/build/page.tsx
@@ -1,24 +1,18 @@
 import BuildProductList from "@/components/build/List";
 import BuildValidateForm from "@/components/build/Form";
-import BuildProductSummary from "@/components/build/Summary";
 import BuildResultList from "@/components/build/Result";
-import { ColumnWrapper, RowWrapper } from "@/components/utils/FlexWrapper";
+import { ColumnWrapper } from "@/components/utils/FlexWrapper";
 
 export default function build() {
   return (
-    <RowWrapper className="w-full">
-      <ColumnWrapper className="w-1/3">
-        <h1 className="text-2xl font-bold mb-4">Build Your PC</h1>
-        <p className="mb-4">
-          Select the components you want to include in your build.
-        </p>
-        <BuildProductList />
-        <BuildValidateForm />
-        <BuildResultList />
-      </ColumnWrapper>
-      <ColumnWrapper>
-        <BuildProductSummary></BuildProductSummary>
-      </ColumnWrapper>
-    </RowWrapper>
+    <ColumnWrapper className="w-full">
+      <h1 className="text-2xl font-bold mb-4">Build Your PC</h1>
+      <p className="mb-4">
+        Select the components you want to include in your build.
+      </p>
+      <BuildProductList />
+      <BuildValidateForm />
+      <BuildResultList />
+    </ColumnWrapper>
   );
 }
