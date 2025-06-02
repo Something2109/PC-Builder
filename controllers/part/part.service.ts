4c056d1693675cb8a2ba8d056476d63cdf1caec5
Hieu Duong <hieuduong21092002@outlook.com>
2025-06-02 14:57:26 +0700

    fix: create new product without part attribute

--- controllers/part/part.service.ts
+++ controllers/part/part.service.ts
@@ -64,7 +64,7 @@ class PartService implements PartServiceInterface {
   async create(product: Products, data: Part.Detail) {
     try {
       const instance = await this.CRUDService.create(
-        data,
+        { ...data, part: product },
         product ? Mapping.Info[product] : undefined
       );
 
