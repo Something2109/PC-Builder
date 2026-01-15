a52341ae1540e9915bf5200fdc6b55a81992c4f9
HIEU Duong (VTI.D5) <hieu.duong@vti.com.vn>
2026-01-15 10:43:50 +0700

    refactor(nextjs): change the jwt package to jose

--- components/auth-server.tsx
+++ components/auth-server.tsx
@@ -1,9 +1,9 @@
 import { Tokens } from "@/utils/API";
 import { JwtPayload } from "@/utils/user";
-import { createVerifier } from "fast-jwt";
+import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
 
-const verify = createVerifier({ key: process.env.JWT_SECRET! });
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export async function verifyToken(): Promise<JwtPayload | null> {
   const cookie = await cookies();
@@ -22,9 +22,9 @@ export async function verifyToken(): Promise<JwtPayload | null> {
   if (bearer !== "Bearer") return null;
 
   try {
-    const payload = verify(token);
+    const { payload } = await jwtVerify(token, secret);
 
-    return payload?.sub as JwtPayload;
+    return payload?.sub as unknown as JwtPayload;
   } catch (err) {
     console.error(err);
     return null;
