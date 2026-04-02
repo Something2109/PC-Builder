1313e0fbd28cd763179c29af5bc537c4e73451d2
Something2109 <hieuduong21092002@outlook.com>
2026-04-02 18:07:36 +0000

    feat: migrate to npm workspace monorepo structure and update Docker configurations

--- apps/crawler/crawler.ts
+++ apps/crawler/crawler.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import fs from "fs";
-import { Products } from "@/utils/Enum";
+import { Products } from "@pc-builder/shared";
 
 type BaseLink = {
   url: URL;
---
+++ packages/shared/index.ts
@@ -0,0 +1,10 @@
+export * from "./API";
+export * from "./Units";
+export * as Part from "./part";
+export { Products } from "./part";
+export * as Article from "./article";
+export * as Build from "./build";
+export * as Interface from "./interface";
+export * as Retailer from "./retailer";
+export * as User from "./user";
+export * from "./utils";
