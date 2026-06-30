1715d9561cc7fd0f853995fff59dad5904e24f7d
HIEU Duong (VTI.D5) <hieu.duong@vti.com.vn>
2026-06-30 08:58:10 +0700

    feat: add custom hooks useClickOutside, useDebounce, and useInfiniteData for improved component functionality

---
+++ apps/frontend/src/hooks/useClickOutside.ts
@@ -0,0 +1,14 @@
+import { RefObject, useEffect } from "react";
+
+// Click outside helper hook
+export function useClickOutside(ref: RefObject<HTMLDivElement | null>, callback: () => void) {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [ref, callback]);
+}
---
+++ apps/frontend/src/hooks/useInfiniteData.ts
@@ -0,0 +1,26 @@
+import axiosInstance from "@/lib/axios";
+import { Payload } from "@pc-builder/shared/API";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+export function useInfiniteData<T>(path: string, context: URLSearchParams) {
+  const query = useInfiniteQuery({
+    queryKey: [path, context.toString()],
+    queryFn: async ({ pageParam = 1 }) => {
+      const params = new URLSearchParams(context);
+      params.set("page", String(pageParam));
+      params.set("limit", "20");
+
+      const { data: resData } = await axiosInstance.get<Payload<T>>(path, { params });
+      return resData;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, _, lastPageParam) => {
+      return lastPageParam >= lastPage.total ? lastPageParam + 1 : undefined;
+    },
+  });
+
+  return useMemo(() => {
+    return { ...query, data: query.data ? query.data.pages.map((res) => res.list).flat() : [] };
+  }, [query]);
+}
