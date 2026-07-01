413731e884e2f59df0929ee4905e1df4b7e186dd
HIEU Duong (VTI.D5) <hieu.duong@vti.com.vn>
2026-07-01 13:47:29 +0700

    refactor: enhance token retrieval functions to handle undefined cookies safely

--- apps/backend/src/utils/auth/tokens.ts
+++ apps/backend/src/utils/auth/tokens.ts
@@ -2,11 +2,11 @@ import { Tokens } from "@pc-builder/shared/API";
 import { Request } from "express";
 
 function getAccessToken(request: Request) {
-  return request.cookies[Tokens.ACCESS];
+  return request.cookies?.[Tokens.ACCESS] ?? "";
 }
 
 function getRefreshToken(request: Request) {
-  return request.cookies[Tokens.REFRESH];
+  return request.cookies?.[Tokens.REFRESH] ?? "";
 }
 
 export { getAccessToken, getRefreshToken };
